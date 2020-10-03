7e99adf9ff6363c874f30ddcee8de213d526fbac
Raiishin <gavin.tanjy11@gmail.com>
2020-10-03 16:58:58 +0800

    Added new changes to the bot's reply

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Telegram Group Chat Notifier
 
 ## How To Use
+
 1. Add the bot into your telegram group/channel
 2. Start a conversation with the bot and get the msg.chat.id
-3. Replace the msg.chat.id with my_telegram_chatId in config/index.js
+3. Replace the relevant fields in config/index.js
 
 ### Now you will start to receive notifications from the bot
--- app/index.js
+++ app/index.js
@@ -2,27 +2,28 @@ const TelegramBot = require("node-telegram-bot-api");
 const env = require("../config");
 const bot = new TelegramBot(env.bot_token, { polling: true });
 
-// Send Changes To Myself
-bot.on("message", (msg) => {
+bot.on("new_chat_members", (msg) => {
   bot.sendMessage(env.my_telegram_chatId, `Original message JSON : ${JSON.stringify(msg)}`);
 
   if (msg.chat.title === env.telegram_group || env.telegram_group_1) {
     const date = msg.date;
-    const memberId = msg.from.id;
     const memberFirstName = msg.from.first_name;
+    const memberLastName = msg.from.last_name;
     const memberUsername = msg.from.username;
 
-    if (msg.left_chat_member) {
-      bot.sendMessage(
-        env.my_telegram_chatId,
-        `${memberFirstName}(Username:${memberUsername} ID:${memberId}) left ${msg.chat.title} on ${date}`
-      );
-    }
-
+    // When members join
     if (msg.new_chat_member) {
       bot.sendMessage(
         env.my_telegram_chatId,
-        `${memberFirstName}(Username:${memberUsername} ID:${memberId}) joined ${msg.chat.title} on ${date}`
+        `${memberFirstName} ${memberLastName}(@${memberUsername}) joined ${msg.chat.title} on ${date}`
+      );
+    }
+
+    // When members leave
+    if (msg.left_chat_member) {
+      bot.sendMessage(
+        env.my_telegram_chatId,
+        `${memberFirstName} ${memberLastName}(@${memberUsername}) left ${msg.chat.title} on ${date}`
       );
     }
   }
@@ -38,5 +39,13 @@ bot.on("message", (msg) => {
     bot.sendMessage(msg.chat.id, "Hello!");
   } else if (message === "i love you") {
     bot.sendMessage(msg.chat.id, "I love you too");
+  }
+});
+
+// Get Stock Prices
+bot.on("message", (msg) => {
+  const message = msg.text.toLowerCase();
+  if (message.includes("$")) {
+    // Call API
   }
 });
